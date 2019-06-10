4af9abfe923866b958e8ef61c2cf666d72e9dfda
Rien van Velzen <rienvanvelzen@gmail.com>
2019-06-10 22:06:27 +0200

    Add express so we can keep the app alive on Heroku

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 require('dotenv').config();
+
+import express from 'express';
+
 import { DiscordJsClient } from "./Discord/Clients/DiscordJsClient";
 import commands from './Discord/Commands';
 
+const app: express.Application = express();
 const client = new DiscordJsClient();
 
 client.addOnMessageListener((msg) => {
@@ -16,3 +20,11 @@ client.addOnMessageListener((msg) => {
 });
 
 client.connect(process.env.DISCORD_TOKEN!);
+
+app.get('/', function (req, res) {
+    res.send('Greetings');
+});
+
+app.listen(80, () => {
+    console.log('App listening on port 80');
+});
