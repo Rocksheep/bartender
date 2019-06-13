2fbce2b4312401434c6e9d86b36e712f9737ae85
Rien van Velzen <rienvanvelzen@gmail.com>
2019-06-13 20:13:51 +0200

    Add a test for Hype

---
+++ tests/unit/HypeTest.spec.ts
@@ -0,0 +1,15 @@
+import { Hype } from "@/Discord/Commands/Hype";
+
+describe('Hype command', () => {
+    it('returns empty content', () => {
+        const command = new Hype();
+        expect(command.getContent()).toBe('');
+    });
+
+    it('returns a file', () => {
+        const command = new Hype();
+        const options = command.getOptions();
+
+        expect(Object.keys(options)).toContain('file');
+    })
+});
